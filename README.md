80bfdf757300ffb6ad8b42aa14e4c17cfaa282ee
FrancoisBasset <mrfrancoisbasset@gmail.com>
2023-09-26 20:29:42 +0200

    Update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Cordova Test
+# Cordova Boilerplate
 
 This template should help get you started developing with Vue 3 in Vite.
 
---
+++ test/test.test.js
@@ -0,0 +1,10 @@
+import { describe, it } from 'mocha';
+import assert from 'assert';
+
+describe('getResult', function() {
+	it('should be true', function() {
+		const b = true;
+
+		assert.equal(b, true);
+	});
+});
